765fc096693d076a7c37c015ed57d4f41e591551
takanotume24 <takanotume24@gmail.com>
2020-02-14 18:53:23 +0900

    content.jsつかう､うごいた

--- background.js
+++ background.js
@@ -1,30 +1,17 @@
-/* The function that finds and returns the selected text */
-const funcToInject = function () {
-    const selection = window.getSelection();
-    return (selection.rangeCount > 0) ? selection.toString() : '';
-};
-
-/* This line converts the above function to string
- * (and makes sure it will be called instantly) */
-const jsCodeStr = ';(' + funcToInject + ')();';
-
 chrome.commands.onCommand.addListener(function (cmd) {
     switch (cmd) {
         case "selected_text":
-            chrome.tabs.executeScript({
-                code: jsCodeStr,
-                allFrames: true   //  <-- inject into all frames, as the selection 
-            }, function (selected_text_per_frame) {
-                console.log(selected_text_per_frame)
-                if (!selected_text_per_frame.length[0]) {
-                    console.log("nothing selected.");
-                    return -1;
-                };
-                if (typeof (selected_text_per_frame[0]) !== "string") { return -1 };
-                if (chrome.runtime.lastError) {
-                    console.log('ERROR:\n' + chrome.runtime.lastError.message);
-                }
-                search(selected_text_per_frame[0])
+            var querry_info = {
+                active: true,
+                windowId: chrome.windows.WINDOW_ID_CURRENT
+            };
+
+            chrome.tabs.query(querry_info, function (result) {
+                var current_tab = result.shift();
+                var message = {};
+                chrome.tabs.sendMessage(current_tab.id, message, function (response) {
+                    search(response.selection_text);
+                });
 
             });
             break;
