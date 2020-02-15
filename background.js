3d89da45b56509c5e8c3379fd7322af33fde017b
takanotume24 <takanotume24@gmail.com>
2020-02-15 19:16:02 +0900

    動的にpopupを生成する

--- background.js
+++ background.js
@@ -6,17 +6,18 @@ chrome.commands.onCommand.addListener(function (cmd) {
                 windowId: chrome.windows.WINDOW_ID_CURRENT
             };
 
-            chrome.tabs.query(querry_info, function (result) {
-                var current_tab = result.shift();
+            (async () => {
+                const result = await browser.tabs.query(querry_info);
+                const current_tab = result.shift();
                 var message = {};
-                chrome.tabs.sendMessage(current_tab.id, message, function (response) {
-                    search(response.selection_text);
-                });
+                const response = await browser.tabs.sendMessage(current_tab.id, message);
+                search(response.selection_text);
+            })();
 
-            });
             break;
     }
 });
+
 
 chrome.contextMenus.create({
     title: "検索: '%s'",
@@ -29,17 +30,24 @@ function onRequest(info, tab) {
 };
 
 function search(query) {
-    let options = {
-        engine: "mnrate"
-    };
     (async () => {
-        const result = await browser.storage.sync.get(options)
-        const site_name = await result.engine
-        const site_url = select_url(site_name) + query;
+        const result = await browser.storage.sync.get()
+        const site_name = await result.selected_site
+        const site = await get_url(result, site_name)
+        console.log(site)
+        const site_url = site.url + query;
         chrome.tabs.create({ url: site_url });
-    })()
+    })();
 }
 
+function get_url(json, site_name) {
+    console.log(json)
+    const match_data = json["engines"].filter(function (item, index) {
+        console.log(item.site_name)
+        if (item.site_name == site_name) return true
+    })
+    return match_data[0]
+}
 function select_url(site_name) {
     switch (site_name) {
         case "mnrate":
--- popup.js
+++ popup.js
@@ -1,11 +1,77 @@
 $(function () {
-    $('input[name="optionsRadios"]:radio').change(function () {
-        const site_name = $(this).val()
-        const options = {
-            engine: site_name
-        }
-        chrome.storage.sync.set(options, function () {
-            console.log("is set:" + options.engine);
+    $(document).ready(function () {
+        chrome.storage.sync.get(function (result) {
+            console.log(result)
+            for (const i in result["engines"]) {
+                $(".col-sm-6").append(`
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios1" value=${result["engines"][i].site_name}> ${result["engines"][i].display_name}
+                        </label>
+                    </div> 
+                `)
+            }
+            $('input[name="optionsRadios"]:radio').val([result.selected_site])
         });
-    })
+
+    });
+
+    $(document).on("change", 'input[name="optionsRadios"]:radio', function () {
+        const site_name = $(this).val()
+        let option_data = null
+        chrome.storage.sync.get(function (result) {
+            option_data = result
+            console.log(`from sync:${option_data.selected_site}`)
+        })
+
+        if (!option_data) { option_data = get_default_option_data(); }
+
+        option_data.selected_site = site_name;
+        console.log(option_data);
+
+        chrome.storage.sync.set(option_data, function () {
+            console.log(option_data);
+        })
+    });
 });
+
+function check_first_instration() {
+    (async () => {
+        const result = await browser.storage.sync.get()
+        if (result) {
+            return true
+        } else {
+            return false
+        }
+    })
+}
+
+function get_default_option_data() {
+    console.log(`first instration`)
+    return {
+        engines: [
+            {
+                display_name: "ラクマ",
+                site_name: "rakuma",
+                url: "https://fril.jp/search/"
+            }, {
+                display_name: "Amazon",
+                site_name: "amazon",
+                url: "https://www.amazon.co.jp/s?k="
+            }, {
+                display_name: "メルカリ",
+                site_name: "mercari",
+                url: "https://www.mercari.com/jp/search/?keyword="
+            }, {
+                display_name: "ヤフオク",
+                site_name: "yahuoku",
+                url: "https://auctions.yahoo.co.jp/search/search?auccat=&tab_ex=commerce&ei=utf-8&aq=-1&oq=&sc_i=&fr=auc_top&p="
+            }, {
+                display_name: "モノレート",
+                site_name: "mnrate",
+                url: "https://mnrate.com/search?i=All&kwd="
+            }
+        ],
+        selected_site: "mnrate"
+    }
+}
