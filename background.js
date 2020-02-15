cccff513158d1f5a19b05d6262e1550f7a00a197
takanotume24 <takanotume24@gmail.com>
2020-02-16 01:12:07 +0900

    とりあえず動いてる気がする…

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
-chrome.commands.onCommand.addListener(function (cmd) {
+document.write("<script src=\"helper.js\"></script> ");
+
+chrome.commands.onCommand.addListener(async function (cmd) {
     switch (cmd) {
         case "selected_text":
-            var querry_info = {
+            let querry_info = {
                 active: true,
                 windowId: chrome.windows.WINDOW_ID_CURRENT
             };
 
-            (async () => {
-                const result = await browser.tabs.query(querry_info);
-                const current_tab = result.shift();
-                var message = {};
-                const response = await browser.tabs.sendMessage(current_tab.id, message);
-                search(response.selection_text);
-            })();
+            const result = await browser.tabs.query(querry_info);
+            const current_tab = result.shift();
+            let message = {};
+            const response = await browser.tabs.sendMessage(current_tab.id, message);
+            await search(response.selection_text);
 
             break;
     }
@@ -29,21 +29,17 @@ function onRequest(info, tab) {
     search(info.selectionText);
 };
 
-function search(query) {
-    (async () => {
-        const result = await browser.storage.local.get()
-        const display_name = await result.selected_site
-        const site = await get_url(result, display_name)
-        console.log(site)
-        const site_url = site.url + query;
-        chrome.tabs.create({ url: site_url });
-    })();
+async function search(query) {
+    const result = await get_sites()
+    const display_name = await result.selected_site
+    const site = await get_url(result, display_name)
+    const replased_url = site.url.replace("STRING", query)
+    await browser.tabs.create({ url: replased_url });
 }
 
-function get_url(json, display_name) {
+async function get_url(json, display_name) {
     console.log(json)
     const match_data = json["engines"].filter(function (item, index) {
-        console.log(item.display_name)
         if (item.display_name == display_name) return true
     })
     return match_data[0]
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
+document.write("<script src=\"helper.js\"></script> ");
+
 $(function () {
-    $(document).ready(function () {
-        chrome.storage.local.get(function (result) {
-            console.log(result)
-            for (const i in result["engines"]) {
-                $(".col-sm-6").append(`
+    $(document).ready(async function () {
+        const result = await get_sites()
+        for (const i in result["engines"]) {
+            $(".col-sm-6").append(`
                 <tr>
                 <td>
                     <div class="radio">
@@ -15,50 +16,15 @@ $(function () {
 
                 </tr>
                     `)
-            }
-            $('input[name="optionsRadios"]:radio').val([result.selected_site])
-        });
-
+        }
+        $('input[name="optionsRadios"]:radio').val([result.selected_site])
     });
 
-    $(document).on("change", 'input[name="optionsRadios"]:radio', function () {
+    $(document).on("change", 'input[name="optionsRadios"]:radio', async function () {
         const display_name = $(this).val()
-        let option_data = null
-        chrome.storage.local.get(function (result) {
-            option_data = result
-            console.log(`from sync:${option_data.selected_site}`)
-        })
-
-        if (!option_data) { option_data = get_default_option_data(); }
+        let option_data = await get_sites()
 
         option_data.selected_site = display_name;
-        console.log(option_data);
-
-        chrome.storage.local.set(option_data, function () {
-            console.log(option_data);
-        })
+        await set_sites(option_data)
     });
 });
-
-
-let default_option_data = {
-    engines: [
-        {
-            display_name: "ラクマ",
-            url: "https://fril.jp/search/"
-        }, {
-            display_name: "Amazon",
-            url: "https://www.amazon.co.jp/s?k="
-        }, {
-            display_name: "メルカリ",
-            url: "https://www.mercari.com/jp/search/?keyword="
-        }, {
-            display_name: "ヤフオク",
-            url: "https://auctions.yahoo.co.jp/search/search?auccat=&tab_ex=commerce&ei=utf-8&aq=-1&oq=&sc_i=&fr=auc_top&p="
-        }, {
-            display_name: "モノレート",
-            url: "https://mnrate.com/search?i=All&kwd="
-        }
-    ],
-    selected_site: "モノレート"
-}
